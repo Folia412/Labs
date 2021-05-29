d840cefea8730b86d3819bb0e775a52e01b1656c
Folia412 <79314786+Folia412@users.noreply.github.com>
2021-05-29 17:54:44 +0300

    Improvement3

--- lab1/src/main/scala/main.scala
+++ lab1/src/main/scala/main.scala
@@ -2,21 +2,17 @@ import scala.language.implicitConversions
 
 trait CommutativeMonoid[A]{
   def union(a1: A, a2: A): A
-  def sum(a1: A, a2: A): A
   def zero: A
-  }
-  object CommutativeMonoid{
-    implicit val intCommutativeMonoid = new CommutativeMonoid[Set[Int]] {
-      override def union(x: Set[Int], y: Set[Int]):
-      Set[Int] = x | y
-      override def sum(x: Set[Int], y: Set[Int]):
-      Set[Int] = x ++ y
-      override val zero = Set()
-    }
 }
-class CommutativeMonoidOps[A](x: A)(implicit g: CommutativeMonoid[A]) {
-  def ||(y: A): A = g.union(x, y)
-  def **(y: A): A = g.sum(x, y)
+object CommutativeMonoid{
+  implicit val intCommutativeMonoid = new CommutativeMonoid[Set[Int]] {
+    override def union(x: Set[Int], y: Set[Int]):
+    Set[Int] = x | y
+    override val zero = Set()
+  }
+}
+class CommutativeMonoids[A: CommutativeMonoid](x: A)(implicit g: CommutativeMonoid[A]) {
+  def ||(y: A): A = g.union(x, y)  
 }
 object CommutativeMonoidOps {
   implicit def common[A](x: A)(implicit g: CommutativeMonoid[A]): CommutativeMonoidOps[A] = {
--- lab1/src/test/scala/CommutativeMonoidSpecification.scala
+++ lab1/src/test/scala/CommutativeMonoidSpecification.scala
@@ -17,29 +17,9 @@ object MonoidSpecification extends Properties("CommutativeMonoid") {
     }
     property("identelem1") = forAll {
       (a: Set[Int]) =>
-      var b : Set[Int] = Set()
-      var s1 : Set[Int] = a || b
-      var s2 : Set[Int] = b || a
-      s1 == a
-      s2 == a
-    }
-    property("associat2") = forAll {
-      (a: Set[Int], b: Set[Int], c: Set[Int]) =>
-      var s1 : Set[Int] = (a ** b) ** c
-      var s2 : Set[Int] = a ** (b ** c)
-      s1 == s2
-    }
-    property("commut2") = forAll {
-      (a: Set[Int], b: Set[Int]) =>
-      var s1 : Set[Int] = a ** b
-      var s2 : Set[Int] = b ** a
-      s1 == s2
-    }
-    property("identelem2") = forAll {
-      (a: Set[Int]) =>
-      var b : Set[Int] = Set()
-      var s1 : Set[Int] = a ** b
-      var s2 : Set[Int] = b ** a
+      val z : Set[Int] = CommutativeMonoid.intCommutativeMonoid.zero
+      var s1 : Set[Int] = a || z
+      var s2 : Set[Int] = z || a
       s1 == a
       s2 == a
     }
