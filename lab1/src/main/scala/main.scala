1bceb52c00e30545044001da5baca068fefc73a2
Folia412 <79314786+Folia412@users.noreply.github.com>
2021-05-29 14:58:25 +0300

    Improvement

--- lab1/src/main/scala/main.scala
+++ lab1/src/main/scala/main.scala
@@ -1,8 +1,8 @@
 import scala.language.implicitConversions
 
 trait CommutativeMonoid[A]{
-  def union(a1: A, a2: A):A
-  def sum(a1: A, a2: A):A
+  def union(a1: A, a2: A): Set[Int]
+  def sum(a1: A, a2: A): Set[Int]
 }
 object CommutativeMonoid{
   implicit val intCommutativeMonoid = new CommutativeMonoid[Set[Int]] {
@@ -13,12 +13,11 @@ object CommutativeMonoid{
   }
 }
 class CommutativeMonoidOps[A](x: A)(implicit g: CommutativeMonoid[A]) {
-  def ||(y: A): A = g.union(x, y)
-  def **(y: A): A = g.sum(x, y)
+  def ||(y: A): Set[Int] = g.union(x, y)
+  def **(y: A): Set[Int] = g.sum(x, y)
 }
 object CommutativeMonoidOps {
   implicit def common[A](x: A)(implicit g: CommutativeMonoid[A]): CommutativeMonoidOps[A] = {
     new CommutativeMonoidOps[A](x)
   }
 }
-    
